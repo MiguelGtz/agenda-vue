a17af93f50d36e5bb1b8c9dc37a02b29149fe399
Miguel Gutiérrez <imickey2010@hotmail.com>
2021-05-17 22:55:33 -0500

    etiquetas modificadas a router-link

--- src/components/Navbard.vue
+++ src/components/Navbard.vue
@@ -2,11 +2,13 @@
   <div class="barra mb-3">
     <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/homed"><i class="fas fa-home"></i></a>
+        <router-link class="navbar-brand" to=""
+          ><i class="fas fa-home"></i
+        ></router-link>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="/consultas">Consultas</a>
+              <router-link class="nav-link" to="/consultas">Consultas</router-link>
             </li>
           </ul>
           <div class="d-flex">

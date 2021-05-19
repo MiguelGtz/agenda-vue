a6d72988232091c9c90cd8f1dc18256738fe148c
Miguel Gutiérrez <imickey2010@hotmail.com>
2021-05-19 16:50:38 -0500

    Homes Modificados

--- src/views/Homed.vue
+++ src/views/Homed.vue
@@ -1,15 +1,45 @@
 <template>
   <Navbard />
+  <div class="homed">
+    <div class="alert alert-light" role="alert">
+      <h1 class="bienvenida">Bienvenido, {{ usuario }}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbard from "../components/Navbard";
+import firebase from "firebase";
 
 export default {
   components: {
     Navbard,
   },
+  created() {
+    const user = firebase.auth().currentUser;
+    if (user) {
+      this.usuario = user.email.split("@")[0];
+    }
+  },
+  data() {
+    return {
+      usuario: "",
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.homed {
+  background-image: url("../assets/background-homed.png");
+  background-size: 100%;
+  background-position: 50% 50%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bienvenida {
+  font-size: 100px;
+}
+</style>
--- src/views/Homep.vue
+++ src/views/Homep.vue
@@ -1,15 +1,45 @@
 <template>
   <Navbarp />
+  <div class="homep">
+    <div class="alert alert-light" role="alert">
+      <h1 class="bienvenida">Bienvenido, {{ usuario }}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbarp from "../components/Navbarp";
+import firebase from "firebase";
 
 export default {
   components: {
     Navbarp,
   },
+  created() {
+    const user = firebase.auth().currentUser;
+    if (user) {
+      this.usuario = user.email.split("@")[0];
+    }
+  },
+  data() {
+    return {
+      usuario: "",
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.homep {
+  background-image: url("../assets/background-homep.png");
+  background-size: 100%;
+  background-position: 50% 50%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bienvenida {
+  font-size: 100px;
+}
+</style>
